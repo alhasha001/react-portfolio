63e9b7425b3f788252231772cbf7a0878f112e3c
Ammar <ammar@growficient.com>
2021-02-17 02:16:22 +0100

    chore: Update navbar text

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,7 +11,7 @@ const Navbar = () => {
             className="name"
             tabIndex={0}
           >
-            Portfolio.
+            Ammar Alhashmi
           </div>
           <div className="links-wrapper">
             <button onClick={() => scrollTo("#work")}>Work</button>
