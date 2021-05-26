6c069766e7b495d757f4e904d1bcf1f1debfc82a
Ammar <ammar@growficient.com>
2021-05-26 20:26:59 +0200

    foto

--- src/yourdata.js
+++ src/yourdata.js
@@ -84,7 +84,7 @@ export default {
   aboutParaThree:
   "During my Education, I learned software engineering skills and principles. I also did a couple of Internships which help gain more knowledge as a full-stack engineer.",
   aboutImage:
-    "https://media-exp1.licdn.com/dms/image/C4D03AQE9n8-bSTQf3w/profile-displayphoto-shrink_800_800/0/1616037537624?e=1621468800&v=beta&t=oDte14anYgc5VuEbeZRNO1IB5kw83GXgr4XzpwKafdA",
+    "https://media-exp1.licdn.com/dms/image/C4D03AQE9n8-bSTQf3w/profile-displayphoto-shrink_800_800/0/1616037537624?e=1627516800&v=beta&t=juN3GxZruVJQFoQgHAl6SnmhVFEC-HtiQfUasakRn0c",
 
   //   End About Section ---------------------
 
